b07b690ddfc74747a7c0aa6af972e8d44a4bea87
Tiffany <tiffany@tiffanywhite.dev>
2023-02-07 03:26:02 -0500

    Continue to setup auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,23 +1,21 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/provider/discord";
+import DiscordProvider from "next-auth/providers/discord";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "lib/mongodb";
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
       profile(profile) {
         return {
           id: profile.id.toString(),
           name: profile.name || profile.login,
           username: profile.login,
           email: profile.email,
-          image: profile.avatar_url,
-          followers: profile.followers,
-          verified: true,
+          mfa_enabled: true
         };
       },
     }),
