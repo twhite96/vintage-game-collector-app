0b526105bfccd7ba008036a905deaeb0f14a630b
Tiffany <tiffany@tiffanywhite.dev>
2023-02-08 02:15:49 -0500

    Create feature branch. Add files to set up Next Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,16 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import clientPromise from "../../../lib/mongodb";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "lib/mongodb";
 
+
+// For more information on each option (and a full list of options) go to
+// https://authjs.dev/reference/configuration/auth-options
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: "my-data-base-name",
+  }),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
@@ -15,7 +21,7 @@ export default NextAuth({
           name: profile.name || profile.login,
           username: profile.login,
           email: profile.email,
-          mfa_enabled: true
+          mfa_enabled: true,
         };
       },
     }),
@@ -28,3 +34,8 @@ export default NextAuth({
     },
   },
 });
+
+// export default NextAuth({
+//   adapter: MongoDBAdapter(clientPromise),
+  
+// });
